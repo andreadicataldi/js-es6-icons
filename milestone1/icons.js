2250787766e2f049ff46f88f2d73e376b1bb16b4
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2020-12-10 17:36:43 +0100

    Milestone 1 Final
    Completed

--- milestone1/icons.js
+++ milestone1/icons.js
@@ -17,15 +17,31 @@ $(document).ready(function () {
     new Icon("horse", "fas", "fa-horse", "animals"),
     new Icon("hippo", "fas", "fa-hippo", "animals"),
     new Icon("fish", "fas", "fa-fish", "animals"),
+    new Icon("carrot", "fas", "fa-carrot", "fruits & vegetables"),
+    new Icon("apple", "fas", "fa-apple-alt", "fruits & vegetables"),
+    new Icon("lemon", "fas", "fa-lemon", "fruits & vegetables"),
+    new Icon("pepper", "fas", "fa-pepper-hot", "fruits & vegetables"),
+    new Icon("astronaut", "fas", "fa-user-astronaut", "users & people"),
+    new Icon("graduate", "fas", "fa-user-graduate", "users & people"),
+    new Icon("ninja", "fas", "fa-user-ninja", "users & people"),
+    new Icon("secret", "fas", "fa-user-secret", "users & people"),
   ];
   // Selezioniamo il container icons
-  const iconSection = $(".container .icons");
+  const container = document.getElementById("icons");
   //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
- 
-
+  icons.forEach((icon) => {
+    const markup = `
+    <div>
+       <i class="${icon.prefix} ${icon.type}"></i>
+       <div class="title">${icon.name.toUpperCase()}</div>
+    </div>
+    `;
+    container.insertAdjacentHTML("beforeend", markup);
+  });
 
 /* ---- FUNCTIONS ----*/
 // Creiamo una funzione per iterare nell'array e che appenda al container le icone.
 // tip: possiamo usare un ciclo foreach qui e destrutturare gli le proprieta degli elementi di un array
 // tip: il template literal ci puo aiutare con il markup
 
+});
